427c668dd27872b0609656a0b54e2f083da7cb96
M. Adam Kendall <akendall@devis.com>
2014-05-23 14:09:54 -0400

    Update the sqlite version example so it works

--- examples/dump2sqlite.py
+++ examples/dump2sqlite.py
@@ -6,10 +6,8 @@ This can be set up in a cronjob to dump data daily.
 Create a config file at ~/.fitbit.conf with the following:
 
 [fitbit]
-user_id: 12XXX
-sid: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-uid: 123456
-uis: XXX%3D
+email: example@exampl.org
+password: MyPassword
 dump_dir: ~/Dropbox/fitbit
 db_file: ~/data/nameofdbfile.sqlite
 
@@ -19,7 +17,7 @@ The timestamp in the table is a unix timestamp. Tables are set up so that the sc
 """
 
 from time import mktime, sleep
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from os import path
 import ConfigParser
 import sqlite3
@@ -32,8 +30,8 @@ CONFIG.read(["fitbit.conf", path.expanduser("~/.fitbit.conf")])
 DB_FILE = path.expanduser(CONFIG.get('fitbit', 'db_file'))
 
 def client():
-	return fitbit.Client(CONFIG.get('fitbit', 'user_id'), CONFIG.get('fitbit', 'sid'), CONFIG.get('fitbit', 'uid'), CONFIG.get('fitbit', 'uis'))
-
+	# return fitbit.Client(CONFIG.get('fitbit', 'user_id'), CONFIG.get('fitbit', 'sid'), CONFIG.get('fitbit', 'uid'), CONFIG.get('fitbit', 'uis'))
+	return fitbit.Client.login(CONFIG.get('fitbit', 'email'), CONFIG.get('fitbit', 'password'))
 def create_table(table, db):
 	db.execute("create table %s (datetime integer PRIMARY KEY ON CONFLICT REPLACE, %s integer)" % (table, table))
 	db.execute("create table %s_daily (date integer PRIMARY KEY ON CONFLICT REPLACE, %s integer)" % (table, table))
@@ -60,10 +58,23 @@ def dump_to_db(db, data_type, date, data):
 	db.execute("insert into %s_daily values (?, ?)" % data_type, (mktime(date.timetuple()), sum))
 	db.commit()
 
+def previously_dumped(db, date):
+	params = (mktime(date.timetuple()),)
+	cursor = db.cursor()
+	cursor.execute('select * from steps_daily where date=?', params)
+	if cursor.fetchone():
+		return True
+	return False
+
 def dump_day(db, date):
 	c = client()
 
-	dump_to_db(db, "steps", date, c.intraday_steps(date))
+	steps = c.intraday_steps(date)
+	# Assume that if no steps were recorded then there is no data
+	if sum([s[1] for s in steps]) == 0:
+		return False
+
+	dump_to_db(db, "steps", date, steps)
 	sleep(1)
 	dump_to_db(db, "calories", date, c.intraday_calories_burned(date))
 	sleep(1)
@@ -72,8 +83,13 @@ def dump_day(db, date):
 	dump_to_db(db, "sleep", date, c.intraday_sleep(date))
 	sleep(1)
 
+	return True
+
 if __name__ == '__main__':
 	db = connect_db(DB_FILE)
+
+	date = datetime.now().date()
+	# print date
 
 	#oneday = timedelta(days=1)
 	#day = datetime(2009, 10, 18).date()
@@ -82,6 +98,27 @@ if __name__ == '__main__':
 	#	dump_day(db, day)
 	#	day += oneday
 
-	dump_day(db, (datetime.now().date() - timedelta(days=1)))
+	# print "Previously fetched: %s" % previously_dumped(db, date - timedelta(days=1) )
+	# dump_day(db, (datetime.now().date() - timedelta(days=1)))
+
+	# Look for the most recent sync
+	while (date.today() - date).days < 365:
+		r = dump_day(db, date)
+		date -= timedelta(days=1)
+		if r:
+			break
+
+	if (date.today() - date).days > 365:
+		# No sync in the last year.
+		sys.exit(1)
+
+	while not previously_dumped(db, date):
+		r = dump_day(db, date)
+		date -= timedelta(days=1)
+		if not r:
+			break
+
+	# Always redump the last dumped day because we may have dumped it before the day was finished.
+	dump_day(db, date)
 
 	db.close()
